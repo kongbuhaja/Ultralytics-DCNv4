918a7084691385469e080724e3f4e45ff1904947
kongbuhaja <pcar530@gmail.com>
2024-12-05 18:28:40 +0900

    update train set

--- train.sh
+++ train.sh
@@ -9,19 +9,19 @@ log_directory_check(){
 
 start_background_task() {
     echo "$1"
-    nohup python3 train.py --model "$1" > "$2/$1.log" 2>&1 &
+    nohup python3 train.py --model "$1" --cpus "$2" --gpus "$3" > "$4/$1.log" 2>&1 &
     sleep 10
 }
 
 wait_for_completion() {
-    while pgrep -f "python3 train.py --model $1" > /dev/null; do
+    while pgrep -f "python3 train.py --model $1 --cpus $2 --gpus $3" > /dev/null; do
         sleep 30
     done
 }
 
 task() {
-    start_background_task "$1" "$2" 
-    wait_for_completion "$1"
+    start_background_task "$1" "$2" "$3" "$4"
+    # wait_for_completion "$1"
 }
 
 log_dir="./logs"
@@ -34,15 +34,12 @@ log_directory_check "$log_dir"
 # 2,724,448     2,775,520     2,624,080
 
 
-# task deco-yolo8n "$log_dir" #2,807,136
-# task deco-yolo10n "$log_dir" #2,858,208
-task deco-yolo11n "$log_dir" #2,719,760
+# task deco-yolo8n "0-15" 0 "$log_dir" # 2,731,184
+task deco-yolo10n "0-15" 0 "$log_dir" # 2,782,256
+task deco-yolo11n "16-31" 1 "$log_dir" # 2,660,752
 
-# task psd-yolo10n "$log_dir" #2,775,464
-# task c2psd-yolo11n "$log_dir" #2,623,944
+task psd-yolo10n "32-47" 2 "$log_dir" # 2,780,720
+task c2psd-yolo11n "48-63" 3 "$log_dir" # 2,629,280
 
-# task deco-psd-yolo10n "$log_dir" #2,858,152
-# task deco-c2psd-yolo11n "$log_dir" #2,719,704
-
-# wait_for_completion deco-yolo11n
-# start_background_task deco-yolo11n "$log_dir"
+# task deco-psd-yolo10n "$log_dir" # 2,787,456
+# task deco-c2psd-yolo11n "$log_dir" # 2,665,952
