f1953b5164366719811048569f94dbba9c5f10de
kongbuhaja <pcar530@gmail.com>
2024-12-30 23:41:41 +0900

    update default option

--- ultralytics/nn/modules/conv.py
+++ ultralytics/nn/modules/conv.py
@@ -351,7 +351,7 @@ class DConv(nn.Module):
 class DCN(nn.Module):
     default_act = nn.SiLU()
 
-    def __init__(self, c, k=3, s=1, p=None, d=1, gc=16, dk=3, act=True):
+    def __init__(self, c, k=3, s=1, p=None, d=1, gc=8, dk=3, act=True):
         super().__init__()
         assert k==3
         self.conv = DCNv4(c, k, s, autopad(k, p, d), group=c//gc, dw_kernel_size=dk, without_pointwise=False, output_bias=False)
