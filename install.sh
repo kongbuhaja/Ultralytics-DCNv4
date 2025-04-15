9f7b46d3113675de36d092475e9f85bcd75f9257
An Hyeon Seok <42567320+kongbuhaja@users.noreply.github.com>
2025-04-15 15:26:23 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 # conda activate dcnv4
 
 pip install torch==1.11.0+cu113 torchvision==0.12.0+cu113  -f https://download.pytorch.org/whl/torch_stable.html
-cd DCNv4_op/
+cd ultralytics/nn/modules/DCNv4_op/
 sh make.sh
-cd ../
+cd ../../../../
 
 pip install ultralytics
