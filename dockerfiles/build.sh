786cb384ce396d5834ce84b7bb1874066746d562
kongbuhaja <pcar530@gmail.com>
2025-06-11 03:37:59 +0900

    update dockerfile

--- dockerfiles/build.sh
+++ dockerfiles/build.sh
@@ -1,2 +1,2 @@
-sudo docker build --force-rm -f {PATH}/Ultralytics-DCNv4/dockerfiles/env.dockerfile -t ud:latest .
+sudo docker build --force-rm -f {PATH}/Ultralytics-DCNv4/dockerfiles/env.dockerfile -t ud:latest 
 sudo docker run --gpus '"device=0"' --cpuset-cpus=0-15 -m 64g --shm-size=16g -it -v {PATH}:/home/ML--name ud ud:latest
